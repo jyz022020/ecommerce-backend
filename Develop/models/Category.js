290b4508b8a261042da01be67278dc4274bcf0f8
Yunzhu Jin <jyz022020@gmail.com>
2022-05-08 22:04:22 -0700

    add all models

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -6,7 +6,18 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // define columns Category
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull:false,
+    }
   },
   {
     sequelize,
